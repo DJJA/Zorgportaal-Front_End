c7a84e8fb10f7187779e59e4266cc049bd18d6e3
Dirk-Jan de Beijer <djjadebeijer@gmail.com>
2018-12-11 09:52:19 +0100

    Removed client and mentor to change to lowercase

--- src/app/models/Client.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "./user";
-
-export class Client extends User {
-    
-    carePlan: string;
-}
--- src/app/models/Mentor.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "./user";
-
-export class Mentor extends User {
-    
-    education: string;
-}
